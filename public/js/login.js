d1f063b7c3a0b6913b424264742c906916a8f466
남형석 <azerc@MacMini.local>
2022-08-22 08:57:15 +0900

    add login.js on frontside

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,26 @@
-console.log("login.js");
+const id = document.getElementById("id");
+const pw = document.getElementById("pw");
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", () => {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  console.log(req);
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.result === "success") {
+        window.location.href = "/";
+      } else {
+        alert("아이디 또는 비밀번호가 잘못되었습니다.");
+      }
+    });
+});
