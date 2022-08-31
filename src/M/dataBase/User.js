0cd513185685dc3683e1952e7b817f954e561332
남형석 <azerc@MacMini.local>
2022-09-01 08:38:18 +0900

    register on database

--- src/M/dataBase/User.js
+++ src/M/dataBase/User.js
@@ -4,46 +4,52 @@ class User {
   constructor(body) {
     this.body = body;
   }
-
   async login() {
-    const user = await UserStorage.getUserInfo(this.body.id);
-    console.log(user);
-    if (user === undefined) {
-      return {
-        result: "fail",
-        message: "wrong id",
-      };
-    }
-    if (user.password === this.body.pw) {
-      return {
-        result: "success",
-        message: " login success",
-      };
-    } else {
-      return {
-        result: "fail",
-        message: "wrong password",
-      };
+    try {
+      const user = await UserStorage.getUserInfo(this.body.id);
+      console.log(user);
+      if (user === undefined) {
+        return {
+          result: "fail",
+          message: "wrong id",
+        };
+      }
+      if (user.password === this.body.pw) {
+        return {
+          result: "success",
+          message: " login success",
+        };
+      } else {
+        return {
+          result: "fail",
+          message: "wrong password",
+        };
+      }
+    } catch (err) {
+      return { success: false, msg: err };
     }
   }
-
   async register() {
-    const response = await UserStorage.save(this.body);
-    if (response === undefined) {
-      return {
-        result: "fail",
-        message: "save fail",
-      };
-    } else if (!response.success) {
-      return {
-        result: "fail",
-        message: "id already exist",
-      };
-    } else if (response.success) {
-      return {
-        result: "success",
-        message: "save success",
-      };
+    try {
+      const response = await UserStorage.save(this.body);
+      if (response === undefined) {
+        return {
+          result: "fail",
+          message: "save fail",
+        };
+      } else if (!response.success) {
+        return {
+          result: "fail",
+          message: "id already exist",
+        };
+      } else if (response.success) {
+        return {
+          result: "success",
+          message: "save success",
+        };
+      }
+    } catch (err) {
+      return { success: false, msg: err };
     }
   }
 }
--- src/M/dataBase/UserStorage.js
+++ src/M/dataBase/UserStorage.js
@@ -1,59 +1,25 @@
 import database from "./config/database.js";
 
 class UserStorage {
-  static async getUsers(...args) {
-    const users = await fs
-      .readFile("./src/M/fileSystem/databases/users.json")
-      .then((data) => {
-        return JSON.parse(data);
-      })
-      .catch(console.error);
-    console.log("users", users);
-    const newUsers = args.reduce((newUsers, field) => {
-      // console.log("newUsers", newUsers, "field", field);
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
-    console.log("return - newUsers", newUsers);
-    return newUsers;
-  }
   static async getUserInfo(id) {
     return new Promise((resolve, reject) => {
-      database.query("SELECT * FROM users WHERE id = ?", [id], (err, data) => {
+      const query = `SELECT * FROM users WHERE id = ?`;
+      database.query(query, [id], (err, data) => {
         if (err) reject(err);
         resolve(data[0]);
       });
     });
   }
   static async save(body) {
-    const users = await fs
-      .readFile("./src/M/fileSystem/databases/users.json")
-      .then((data) => {
-        return JSON.parse(data);
-      })
-      .catch(console.error);
-
-    if (!users.id.includes(body.id)) {
-      users.id.push(body.id);
-      users.psword.push(body.pw);
-      users.name.push(body.userName);
-      const response = await fs
-        .writeFile(
-          "./src/M/fileSystem/databases/users.json",
-          JSON.stringify(users)
-        )
-        .then(() => {
-          return {
-            success: true,
-          };
-        })
-        .catch(console.error);
-      return response;
-    } else {
-      return { success: false };
-    }
+    return new Promise((resolve, reject) => {
+      const query = `INSERT INTO users(id, name, password) VALUES(?,?,?)`;
+      database.query(query, [body.id, body.userName, body.pw], (err, data) => {
+        if (err) reject(`${err}`);
+        resolve({
+          success: true,
+        });
+      });
+    });
   }
 }
 
