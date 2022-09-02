ad91135bb2caec9a4151615b75059c962b089f06
남형석 <azerc@MacMini.local>
2022-09-03 08:59:08 +0900

    ignore access.log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+import fs from "fs";
+import morgan from "morgan";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 import HomeRouter from "./router.js";
+
+const accessLogStream = fs.createWriteStream(`${path.resolve()}/access.log`, {
+  flag: "a",
+});
 
 const app = express();
 app.set("view engine", "ejs");
@@ -13,6 +19,12 @@ app.set("views", "./src/V/views");
 app.use(cors());
 app.use(express.json());
 app.use(express.static(`${path.resolve()}/public`));
+app.use(
+  morgan(`common`, {
+    stream: accessLogStream,
+  })
+);
+app.use(morgan(`:method :url :response-time ms :status  / TIME : :date[web]`));
 
 app.use("/", HomeRouter);
 
